f8898d2b4b0ff68878fe137c241c91b7fe0e089e
Cai <cloud_chl@163.com>
2024-03-03 14:22:00 +0800

    增加系统优化方法

--- System initialization.py
+++ System initialization.py
@@ -4,14 +4,21 @@
 
 import subprocess
 
-class NetworkConfigure(object):
+def Usage():
+    print("""
+        1. 配置/修改网卡IP
+        2.系统初始化（适用于CentOS、RedHat）
+    """)
+
+ 
+class NetworkConfigure():
     
     def input_ipaddress(self):
-            new_interface = input('网卡名称：')
-            new_ip = input("新IP地址:")
-            new_netmask = input("新子网掩码:")
-            new_gateway = input("新网关:")
-            self.set_ipaddress(new_interface, new_ip, new_netmask, new_gateway)
+        new_interface = input('网卡名称：')
+        new_ip = input("新IP地址:")
+        new_netmask = input("新子网掩码:")
+        new_gateway = input("新网关:")
+        self.set_ipaddress(new_interface, new_ip, new_netmask, new_gateway)
             
 
     def set_ipaddress(self, interface, ip, netmask, gateway='0.0.0.0'):
@@ -46,8 +53,7 @@ class NetworkConfigure(object):
                 lines.append(f'NETMASK={netmask}\n')
             if not gateway_exists:
                 lines.append(f'GATEWAY={gateway}\n')
-
-            # 将修改后的配置写回文件
+                
             with open(f'/etc/sysconfig/network-scripts/ifcfg-{interface}', 'w') as file:
                  file.writelines(lines)
 
@@ -59,41 +65,96 @@ class NetworkConfigure(object):
             print(f'Error occurred: {e}')
 
 
-def SystemConfigure():
+class SystemConfigure():
+    
     # 关闭系统防火墙
-    subprocess.run(['systemctl', 'disable', 'firewalld'])
-    subprocess.run(['systemctl', 'stop', 'firewalld'])
+    def disable_firewalld(self):
+        subprocess.run(['systemctl', 'disable', 'firewalld'])
+        subprocess.run(['systemctl', 'stop', 'firewalld'])
 
     # 关闭SELINUX
-    subprocess.run(['setenforce', '0'])
-    with open('/etc/selinux/config','r') as file:
-        lines = file.readlines()
-        
-    for i, line in enumerate(lines):
-        if line.startswith('SELINUX='):
-            lines[i] = f'SELINUX=disabled'
+    def disable_selinux(self):
+        subprocess.run(['setenforce', '0'])
+        with open('/etc/selinux/config','r') as file:
+            lines = file.readlines()
+            
+        for i, line in enumerate(lines):
+            if line.startswith('SELINUX='):
+                lines[i] = f'SELINUX=disabled'
 
-    with open('/etc/selinux/config', 'w') as file:
-        file.writelines(lines)
+        with open('/etc/selinux/config', 'w') as file:
+            file.writelines(lines)
 
     # 修改最大文件句柄数限制
-    subprocess.run(['ulimit ', '-n', '65535'])
-    with open('/etc/security/limits.conf', 'a') as file:
-         file.write("* soft nofile 65535\n")
-         file.write("* hard nofile 65536\n")
-         file.write("* soft nproc 4096\n")
-         file.write("* hard nproc 4096\n")
-    
-    with open('/etc/sysctl.conf', 'a') as file:
-         file.write("fs.file-max=655350\n")
-    subprocess.run(['/sbin/sysctl', '-p'])
-     
+    def ulimit(self):
+        subprocess.run(['ulimit ', '-n', '65535'])
+        try:
+            with open('/etc/security/limits.conf', 'a') as file:
+                file.write("* soft nofile 65535\n")
+                file.write("* hard nofile 65536\n")
+                file.write("* soft nproc 4096\n")
+                file.write("* hard nproc 4096\n")
+
+            subprocess.run(['/sbin/sysctl', '-p'])
+
+        except Exception as e:
+            print(f'Error occurred: {e}')
+
+    # 增加内核参数
+    def set_kernel_parameters(self):
+        sysctl_conf_path = '/etc/sysctl.conf'
+
+        params_to_add = [
+            "net.ipv4.tcp_fin_timeout = 30",
+            "net.ipv4.tcp_keepalive_time = 1200",
+            "net.ipv4.tcp_syncookies =1",
+            "net.ipv4.tcp_tw_reuse = 1",
+            "net.ipv4.tcp_tw_recycle =1",
+            "net.ipv4.ip_local_port_range = 10000 65000",
+            "net.ipv4.tcp_max_syn_backlog = 8192",
+            "net.ipv4.tcp_max_tw_buckets = 5000",
+        ]
+
+        try:
+            with open(sysctl_conf_path, 'r') as file:
+                existing_content = file.read()
+            
+            with open(sysctl_conf_path, 'a') as file:
+                for params in params_to_add:
+                    if params not in existing_content:
+                        file.write(params + '\n')
+            print("Parameters added to sysctl.conf successfully.")
+
+            subprocess.run(['/sbin/sysctl', '-p'])
+
+        except FileNotFoundError:
+            print(f'Error: {sysctl_conf_path} not found.')
+        except PermissionError:
+            print(f'Error: Permission denied to access {sysctl_conf_path}.')
+        except Exception as e:
+            print(f'Error occurred: {e}')
+
+
+        
+        
+            
 
 def main():
+    Usage()
+    num = int(input('请输入功能选项：' ))
+    if num == int(1):
+        networkconfigure = NetworkConfigure()
+        networkconfigure.input_ipaddress()
+    elif num == int(2):
+        systemconfigure = SystemConfigure()
+        systemconfigure.disable_firewalld()
+        systemconfigure.disable_selinux()
+        systemconfigure.ulimit()
+        systemconfigure.set_kernel_parameters()
+    else:
+        print("Invalid option.")
+        Usage()
         num = int(input('请输入功能选项：' ))
-        if num == int(1):
-             configure = NetworkConfigure()
-             configure.input_ipaddress()
             
 
 if __name__ == '__main__': 
